52fa82f441cc9a02a3d6fdcc93cf499339053d7a
ranp20 <polancoranp2019@gmail.com>
2022-11-23 10:42:52 -0600

    repair changes into admin

--- admin/views/js/main.js
+++ admin/views/js/main.js
@@ -6,8 +6,36 @@ $(() => {
   mSideBarLBackdop_m.addEventListener("click", function(e){if(e.target === mSideBarLBackdop_m){mSideBarLBackdop_m.classList.remove("active");}});
   // ------------ TOGGLE MENU - USUARIO
   btn_sessuser.addEventListener("click", function(){c_sessuser_m.classList.toggle("show");});
-  // ------------ ITEM SELECCIONADO DEL MENÚ EN CADA PÁGINA - SIDEBARLEFT
+  
+  // ------------ TOGGLE MENU SIDEBARLEFT
+  const navMenu = document.querySelector('div.main-nav');
+  const OpenMenus = [];
+  navMenu.addEventListener('click', (e) =>  {
+    if (!e.target.matches('li.submenu > a')) return
+    e.preventDefault();
 
+    let level = 0
+      , eLI   = e.target.closest('li')
+      , eUL   = e.target.closest('ul')
+      , mOpen = eLI.classList.toggle('show')
+      ;
+    for(;;level++){ // get menu Level
+      eUL = eUL.parentElement.closest('ul')
+      if (!eUL || !eUL.matches('div.main-nav ul')) break
+    }
+    for(let i = OpenMenus.length; (--i) > level;){ // close all < sub levels
+      OpenMenus.pop().classList.remove('show')
+    }
+    if(OpenMenus[level] && OpenMenus[level] != eLI )
+      OpenMenus.pop().classList.remove('show')
+
+    if(mOpen){
+      OpenMenus.push(eLI)  // memorize adding menu opened
+    }else{
+      OpenMenus.pop()     // or remove it. 
+    }
+  });
+  // ------------ ITEM SELECCIONADO DEL MENÚ EN CADA PÁGINA - SIDEBARLEFT
   var url = window.location.pathname;
   var filename = url.substring(url.lastIndexOf('/')+1);
   if(filename == "ajustes" || filename == "banner-principal"){
@@ -17,4 +45,5 @@ $(() => {
     $(".nav-dashAdm--sidenav--c--cList--m--i a").removeClass("active");
     $('.nav-dashAdm--sidenav--c--cList--m--i a[href="' + filename + '"]').addClass("active");
   }
+
 });
